a427074e48dab3f6e0c7588a89b542ba22cf5ffb
jacobblyons <45246570+jacobblyons@users.noreply.github.com>
2019-03-11 09:07:20 -0700

    lots of stufffff

--- HyperCube.ino
+++ HyperCube.ino
@@ -12,21 +12,24 @@ volatile bool micState = false;
 
 #define STRIP_LENGTH 32
 
-#define LEDS_ON_TOP 11
-#define LEDS_ON_SIDES 11
+#define LEDS_ON_TOP 10
+#define LEDS_ON_SIDES 10
 #define LEDS_ON_BOT 10
 
-#define TAN 0
-#define PURPLE 1
-#define PINK 2
-#define GREY 3
+#define TAN 0     // pin 8
+#define PURPLE 1  // pin 9 
+#define PINK 2    // pin 10 
+#define GREY 3    // pin 11
 
 CRGB _rawleds[4][32];
-
+int radius = 0;
 #define DEMO_BUTTON 18  // correlates to interrupt 5
 #define SONAR_BUTTON 19 // correlates to interrupt 4
 #define MIC_BUTTON 20 // correlates to interrupt 3
-
+// pin 8 tan
+// pin 9 purple
+// pin 10 pink
+// pin 11 grey
 void setup()
 {
   Serial.begin(9600);
@@ -45,9 +48,8 @@ void setup()
 
 void loop()
 {
-  
-  testx();
-   
+    spherePattern(radius);
+    radius += (radius +1) % 40;
     if (demoState){
         // demo mode code
     }
@@ -59,76 +61,92 @@ void loop()
     }
 
 }
-void testx() {
-  int delayTime = 100;
-  for(int i = 0 ; i < LEDS_ON_BOT ; i ++){
-    //bot right
-    getLED(i,0,0) = CRGB::Blue;
-    delay(delayTime);
-    FastLED.show();
-    getLED(i,0,0) = CRGB::Red;
-    delay(delayTime);
-    FastLED.show();
 
-    //botleft
-    getLED(i,0,LEDS_ON_BOT-1) = CRGB::Blue;
-    delay(delayTime);
-    FastLED.show();
-    getLED(i,0,LEDS_ON_BOT-1) = CRGB::Red;
-    delay(delayTime);
-    FastLED.show();
-    
-    //topright DEAD LEDS
-    getLED(i,LEDS_ON_SIDES -1,0) = CRGB::Blue;
-    delay(delayTime);
-    FastLED.show();
-    getLED(i,LEDS_ON_SIDES -1,0) = CRGB::Red;
-    delay(delayTime);
-    FastLED.show();
+void testCorners(){
+  // pin 8 strip test
+  getLED(1,0,0) =  CRGB::Blue;
+  getLED(10,0,0) =  CRGB::Blue;
+  getLED(11,1,0) = CRGB::Blue;
+  getLED(11,10,0) = CRGB::Blue;
+  getLED(11,11,1) = CRGB::Blue;
+  getLED(11,11,10) = CRGB::Blue;
+  //pin 9 
+  getLED(11,0,1) = CRGB::Blue;
+  getLED(11,0,10) = CRGB::Blue;
+  getLED(11,1,11) = CRGB::Blue;
+  getLED(11,10,11) = CRGB::Blue;
+  getLED(1,11,11) = CRGB::Blue;
+  getLED(10,11,11) = CRGB::Blue;
+  //pin 10
+  getLED(10,0,11) = CRGB::Blue;
+  getLED(1,0,11) = CRGB::Blue;
+  getLED(0,1,11) = CRGB::Blue;
+  getLED(0,10,11) = CRGB::Blue;
+  getLED(0,11,10) = CRGB::Blue;
+  getLED(0,11,1) = CRGB::Blue;
+  //pin 11
+  getLED(0,0,1) = CRGB::Blue;
+  getLED(0,0,10) = CRGB::Blue;
+  getLED(0,1,0) = CRGB::Blue;
+  getLED(0,10,0) = CRGB::Blue;
+  getLED(1,11,0) = CRGB::Blue;
+  getLED(10,11,0) = CRGB::Blue;
+  FastLED.show();
+}
 
-    //topleft
-    getLED(i,LEDS_ON_SIDES -1,LEDS_ON_TOP -1) = CRGB::Blue;
-    delay(delayTime);
-    FastLED.show();
-    getLED(i,LEDS_ON_SIDES -1,LEDS_ON_TOP -1) = CRGB::Red;
-    delay(delayTime);
-    FastLED.show();
-  }
+void spherePattern(int r) {
+  for(int x = 0; x < 12 ; x++) {for(int y = 0; y < 12 ; y++) {for(int z = 0; z < 12 ; z++) {
+    if(inSphere(x,y,z,0,0,0,r)){
+      if(!hasLED(x,y,z)) break;
+      getLED(x,y,z) = CRGB::Red;
+    }
+    else {
+      if(!hasLED(x,y,z)) break;
+      getLED(x,y,z) = CRGB::Blue;
+    }
+  }}}
+  FastLED.show();    
+  FastLED.delay(20);
+  
+}
+
+bool inSphere(int x, int y, int z, int cx, int cy, int cz, int r){
+  return (pow(x - cx, 2) + pow(y - cy, 2) + pow(z - cz, 2)) < r && (pow(x - cx, 2) + pow(y - cy, 2) + pow(z - cz, 2)) > r-2;
 }
 
 void testAll(){
   int delayTime = 15;
   for (int i = 0 ; i < STRIP_LENGTH; i ++) {
-    _rawleds[0][i] = CRGB::HotPink;
+    _rawleds[0][i] = CRGB::Red;
     FastLED.show();
-    delay(delayTime);
-    _rawleds[0][i] = CRGB::HotPink;
+
+    _rawleds[0][i] = CRGB::Red;
     FastLED.show();
-    delay(delayTime);
+
   }
   for (int i = 0 ; i < STRIP_LENGTH; i ++) {
-    _rawleds[1][i] = CRGB::HotPink;
+    _rawleds[1][i] = CRGB::Red;
     FastLED.show();
-    delay(delayTime);
-    _rawleds[1][i] = CRGB::HotPink;
+
+    _rawleds[1][i] = CRGB::Red;
     FastLED.show();
-    delay(delayTime);
+ 
   }
   for (int i = 0 ; i < STRIP_LENGTH; i ++) {
-    _rawleds[2][i] = CRGB::HotPink;
+    _rawleds[2][i] = CRGB::Red;
     FastLED.show();
-    delay(delayTime);
-    _rawleds[2][i] = CRGB::HotPink;
+
+    _rawleds[2][i] = CRGB::Red;
     FastLED.show();
-    delay(delayTime);
+
   }
   for (int i = 0 ; i < STRIP_LENGTH; i ++) {
-    _rawleds[3][i] = CRGB::HotPink;
+    _rawleds[3][i] = CRGB::Red;
     FastLED.show();
-    delay(delayTime);
-    _rawleds[3][i] = CRGB::HotPink;
+ 
+    _rawleds[3][i] = CRGB::Red;
     FastLED.show();
-    delay(delayTime);
+
   }
 }
 
--- LEDCube.ino
+++ LEDCube.ino
@@ -1,74 +1,119 @@
+// pin 8 tan
+// pin 9 purple
+// pin 10 pink
+// pin 11 grey
+
+
 CRGB &getLED(int x, int y, int z)
 {
-
     //-----------------------------------strips in x direction-----------------------------------
-    //top
-    for (int i = 0; i < LEDS_ON_TOP; i++)
+    for (int i = 1; i < 11; i++)
     {
-        //left
-        if (x == i && y == LEDS_ON_SIDES - 1 && z == LEDS_ON_TOP - 1)
-            return _rawleds[PURPLE][STRIP_LENGTH - 1 - i];
-        //right
-        if (x == i && y == LEDS_ON_SIDES - 1 && z == 0)
-            return _rawleds[GREY][STRIP_LENGTH - LEDS_ON_TOP - 1 + i];
-    }
-
-    //bot
-    for (int i = 0; i < LEDS_ON_BOT; i++)
-    {
-        //left
-        if (x == i && y == 0 && z == LEDS_ON_BOT - 1)
-            return _rawleds[PINK][LEDS_ON_BOT - 1 - i];
-        //right
+      int lightIndex = i -1;
+        //topleft
+        if (x == i && y == 11 && z == 11)
+            return _rawleds[PURPLE][STRIP_LENGTH - 3 - lightIndex];
+        //topright
+        if (x == i && y == 11 && z == 0)
+            return _rawleds[GREY][STRIP_LENGTH - 1 - 10 + lightIndex];
+        //botleft
+        if (x == i && y == 0 && z == 11)
+            return _rawleds[PINK][10 - 1 - lightIndex];
+        //botright
         if (x == i && y == 0 && z == 0){
-              return _rawleds[TAN][i];
+              return _rawleds[TAN][lightIndex];
         }
-          
-           
     }
-
     //-----------------------------------strips in y direction-----------------------------------
-    //top
-    for (int i = 0; i < LEDS_ON_SIDES; i++)
+    for (int i = 1; i < 11; i++)
     {
-        //left
+       int lightIndex = i -1;
+        //topleft
         if (x == 0 && y == i && z == 0)
-            return _rawleds[GREY][LEDS_ON_BOT - 1 + i];
-        //right
-        if (x == LEDS_ON_BOT - 1 && y == i && z == 0)
-            return _rawleds[TAN][LEDS_ON_BOT - 1 + i];
-    }
+            return _rawleds[GREY][10 - 1 + 1 + lightIndex];
+        //topright
+        if (x == 11 && y == i && z == 0)
+            return _rawleds[TAN][10 - 1+ 1 + lightIndex];
 
-    //bot
-    for (int i = 0; i < LEDS_ON_SIDES; i++)
-    {
-        //left
-        if (x == 0 && y == i && z == LEDS_ON_BOT - 1)
-            return _rawleds[PINK][LEDS_ON_BOT - 1 + i];
-        //right
-        if (x == LEDS_ON_BOT - 1 && y == i && z == LEDS_ON_BOT - 1)
-            return _rawleds[PURPLE][LEDS_ON_BOT - 1 + i];
+        //botleft
+        if (x == 0 && y == i && z == 11)
+            return _rawleds[PINK][10 - 1 +1+ lightIndex];
+        //botright
+        if (x == 11 && y == i && z == 11)
+            return _rawleds[PURPLE][10 - 1 + 1 + lightIndex];
     }
     //-----------------------------------strips in z direction-----------------------------------
-    //top
-    for (int i = 0; i < LEDS_ON_TOP; i++)
+    for (int i = 0; i < 11; i++)
     {
-        //left
-        if (x == 0 && y == LEDS_ON_SIDES - 1 && z == i)
-            return _rawleds[PINK][STRIP_LENGTH - 1 - i];
-        //right
-        if (x == LEDS_ON_TOP - 1 && y == LEDS_ON_SIDES - 1 && z == i)
-            return _rawleds[TAN][LEDS_ON_BOT + LEDS_ON_SIDES - 1 + i];
-    }
-
-    //bot
-    for (int i = 0; i < LEDS_ON_TOP; i++)
-    {
-        //left
+       int lightIndex = i -1;
+        //topleft
+        if (x == 0 && y == 11 && z == i)
+            return _rawleds[PINK][STRIP_LENGTH - 3 - lightIndex];
+        //topright
+        if (x == 11 && y == 11 && z == i)
+            return _rawleds[TAN][10 -1 + 1 + 10 - 1 + 1 + lightIndex];
+        //botleft
         if (x == 0 && y == 0 && z == i)
-            return _rawleds[GREY][LEDS_ON_BOT - 1 - i];
-        //right
-        if (x == LEDS_ON_BOT - 1 && y == 0 && z == i)
-            return _rawleds[PURPLE][i];
+            return _rawleds[GREY][10 - 1 - lightIndex];
+        //botright
+        if (x == 11 && y == 0 && z == i)
+            return _rawleds[PURPLE][lightIndex];
     }
+}
+
+bool hasLED(int x, int y, int z){
+  //-----------------------------------strips in x direction-----------------------------------
+    for (int i = 1; i < 11; i++)
+    {
+      int lightIndex = i -1;
+        //topleft
+        if (x == i && y == 11 && z == 11)
+            return true;
+        //topright
+        if (x == i && y == 11 && z == 0)
+            return true;
+        //botleft
+        if (x == i && y == 0 && z == 11)
+            return true;
+        //botright
+        if (x == i && y == 0 && z == 0){
+              return true;
+        }
+    }
+    //-----------------------------------strips in y direction-----------------------------------
+    for (int i = 1; i < 11; i++)
+    {
+       int lightIndex = i -1;
+        //topleft
+        if (x == 0 && y == i && z == 0)
+            return true;
+        //topright
+        if (x == 11 && y == i && z == 0)
+            return true;
+
+        //botleft
+        if (x == 0 && y == i && z == 11)
+            return true;
+        //botright
+        if (x == 11 && y == i && z == 11)
+            return true;
+    }
+    //-----------------------------------strips in z direction-----------------------------------
+    for (int i = 0; i < 11; i++)
+    {
+       int lightIndex = i -1;
+        //topleft
+        if (x == 0 && y == 11 && z == i)
+            return true;
+        //topright
+        if (x == 11 && y == 11 && z == i)
+            return true;
+        //botleft
+        if (x == 0 && y == 0 && z == i)
+            return true;
+        //botright
+        if (x == 11 && y == 0 && z == i)
+            return true;
+    }
+    return false;
 }
